3bed916fcc6877b385bbc5412dd2497cbb8391a8
Anto Pisciolari <antopisciolari@Antos-MacBook-Pro.local>
2024-10-20 16:33:32 +0200

    logout

--- sidebar.py
+++ sidebar.py
@@ -1,13 +1,21 @@
 import streamlit as st
-
+from auth import load_authenticator
+# Load authenticator and config
+authenticator = load_authenticator()
 def showSuperAdmins():
     st.sidebar.page_link('pages/admin_dashboard.py', label='General Dashboard')
     st.sidebar.page_link('pages/progression_dashboard.py', label='Progress Dashboard')
     st.sidebar.page_link('pages/tutor_dashboard.py', label='Tutor Dashboard')
     st.sidebar.page_link('pages/aprendiz_dashboard.py', label='Aprendices Dashboard')
+    with st.sidebar:
+        authenticator.logout("Logout", "sidebar") 
 def showAdmins():
     st.sidebar.page_link('pages/admin_dashboard.py', label='General Dashboard')
     st.sidebar.page_link('pages/progression_dashboard.py', label='Progress Dashboard')
+    with st.sidebar:
+            authenticator.logout("Logout", "sidebar") 
 def showTutors():
     st.sidebar.page_link('pages/tutor_dashboard.py', label='Tutor Dashboard')
     st.sidebar.page_link('pages/aprendiz_dashboard.py', label='Aprendices Dashboard')
+    with st.sidebar:
+            authenticator.logout("Logout", "sidebar")     
